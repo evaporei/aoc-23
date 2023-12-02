85001cd876b8841cf74a032d7fd10fc392a0ae71
Eva Pace <eba.pachi@gmail.com>
2023-12-02 09:11:27 -0300

    1: abstract rust code a bit

--- 1/main.rs
+++ 1/main.rs
@@ -3,46 +3,51 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 struct Parser {
-    lines: io::Lines<io::BufReader<File>>,
     sum: usize,
 }
 
 impl Parser {
-    fn new(lines: io::Lines<io::BufReader<File>>) -> Self {
+    fn new() -> Self {
         Self {
-            lines,
             sum: 0,
         }
     }
 
-    fn parse(mut self) -> usize {
-        for line in self.lines {
-            let line = line.unwrap();
-            let mut n = 0;
-            let mut first = true;
-            let mut last = 0;
-            for ch in line.bytes() {
-                if ch >= 49 && ch <= 57 {
-                    if first {
-                        n += (ch - 48) * 10;
-                        first = false;
-                    }
-
-                    last = ch - 48;
-                }
-            }
-            n += last;
-            self.sum += n as usize;
+    fn parse(mut self, lines: io::Lines<io::BufReader<File>>) -> usize {
+        for line in lines {
+            self.sum += self.parse_line(&line.unwrap());
         }
 
         self.sum
     }
+
+    fn parse_line(&self, line: &str) -> usize {
+        let mut n = 0;
+        let mut first = true;
+        let mut last = 0;
+        for ch in line.bytes() {
+            if self.is_digit(ch) {
+                if first {
+                    n += (ch - 48) * 10;
+                    first = false;
+                }
+
+                last = ch - 48;
+            }
+        }
+        n += last;
+        n as usize
+    }
+
+    fn is_digit(&self, ch: u8) -> bool {
+        ch >= 49 && ch <= 57
+    }
 }
 
 fn main() {
+    let parser = Parser::new();
     let lines = read_lines("./easy_input_part_one").unwrap();
-    let parser = Parser::new(lines);
-    let sum = parser.parse();
+    let sum = parser.parse(lines);
     println!("{sum}");
 }
 
