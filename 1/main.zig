f2f7aad10112bbf9c3985808e2fb1151bd2fc79d
Eva Pace <eba.pachi@gmail.com>
2023-12-02 11:12:15 -0300

    1/zig: back to the beginning, create isDigit

--- 1/main.zig
+++ 1/main.zig
@@ -4,12 +4,16 @@ const numbers = [_][]const u8{ "one", "two", "three", "four", "five", "six", "se
 
 const allocator = std.heap.page_allocator;
 
+fn isDigit(ch: u8) bool {
+    return ch >= 49 and ch <= 57;
+}
+
 pub fn main() !void {
     // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     // defer _ = gpa.deinit();
     // var file = try std.fs.cwd().openFile("input", .{}); // 54951
-    // var file = try std.fs.cwd().openFile("easy_input_part_one", .{}); // 142
-    var file = try std.fs.cwd().openFile("easy_input_part_two", .{}); // 209, 281
+    var file = try std.fs.cwd().openFile("easy_input_part_one", .{}); // 142
+    // var file = try std.fs.cwd().openFile("easy_input_part_two", .{}); // 209, 281
     defer file.close();
 
     var buf_reader = std.io.bufferedReader(file.reader());
@@ -22,54 +26,18 @@ pub fn main() !void {
         var n: u8 = 0;
         var first = true;
         var last: u8 = 0;
-        var currWord = std.ArrayList(u8).init(allocator);
-        defer currWord.deinit();
-        var str_n = std.ArrayList(u8).init(allocator);
-        defer str_n.deinit();
         for (line) |char| {
-            if (char >= 49 and char <= 57) {
-                if (currWord.items.len != 0) {
-                    var i: u8 = 0;
-                    for (numbers) |numText| {
-                        if (std.mem.containsAtLeast(u8, currWord.items, 1, numText)) {
-                            try str_n.append(i + 1 + 48);
-                        }
-                        i += 1;
-                    }
-                }
-                std.debug.print("word before n: {s}\n", .{currWord.items});
-                try currWord.resize(0);
-                // for (currWord.items) |_| {
-                //     _ = currWord.pop();
-                // }
-                // currWord = "";
+            if (isDigit(char)) {
                 if (first) {
                     n += (char - 48) * 10;
                     first = false;
                 }
 
-                try str_n.append(char);
                 last = char - 48;
-            } else {
-                // _ = try std.fmt.bufPrint(currWord, "{c}", .{char});
-                // currWord = try currWord.append(char);
-                try currWord.append(char);
             }
         }
-        if (currWord.items.len != 0) {
-            var i: u8 = 0;
-            for (numbers) |numText| {
-                if (std.mem.containsAtLeast(u8, currWord.items, 1, numText)) {
-                    try str_n.append(i + 1 + 48);
-                }
-                i += 1;
-            }
-        }
-        // var i = try std.fmt.parseInt(u8, currWord.items, 10);
-        std.debug.print("word: {s}\n", .{currWord.items});
-        std.debug.print("str_n: {s}\n", .{str_n.items});
         n += last;
-        std.debug.print("n: {d}\n", .{n});
+        // std.debug.print("n: {d}\n", .{n});
         total += n;
     }
     std.debug.print("{d}\n", .{total});
