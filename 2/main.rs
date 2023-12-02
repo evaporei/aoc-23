0a91f2f254a1c0f068238fe4e5252245c1a764b4
Eva Pace <eba.pachi@gmail.com>
2023-12-02 14:49:31 -0300

    2/rust: finished parsing, still ugly, needs refactor

--- 2/main.rs
+++ 2/main.rs
@@ -8,11 +8,23 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Set {
-    red: u8,
-    green: u8,
-    blue: u8,
+    red: Option<u8>,
+    green: Option<u8>,
+    blue: Option<u8>,
+}
+
+impl Set {
+    fn red(&mut self, amount: u8) {
+        self.red = Some(amount);
+    }
+    fn green(&mut self, amount: u8) {
+        self.green = Some(amount);
+    }
+    fn blue(&mut self, amount: u8) {
+        self.blue = Some(amount);
+    }
 }
 
 #[derive(Debug)]
@@ -25,9 +37,57 @@ impl Game {
     fn parse(line: &str) -> Self {
         let id = line.split(' ').skip(1).next().unwrap();
         let id = id[..id.len() - 1].parse().unwrap(); // remove `:`
+
+        let mut str_sets = line.split(';');
+        let first_set = str_sets.next().unwrap().to_owned();
+        let first_set = first_set.split(':').skip(1).next().unwrap();
+
+        let mut cubes = first_set.split(',');
+
+        let mut set = Set::default();
+
+        while let Some(cube) = cubes.next() {
+            if cube.contains("red") {
+                let amount = cube.split(' ').skip(1).next().unwrap();
+                set.red(amount.parse().unwrap());
+            }
+            if cube.contains("green") {
+                let amount = cube.split(' ').skip(1).next().unwrap();
+                set.green(amount.parse().unwrap());
+            }
+            if cube.contains("blue") {
+                let amount = cube.split(' ').skip(1).next().unwrap();
+                set.blue(amount.parse().unwrap());
+            }
+        }
+
+        let mut sets = vec![set];
+
+        for str_set in str_sets {
+            let mut cubes = str_set.split(',');
+
+            let mut set = Set::default();
+            while let Some(cube) = cubes.next() {
+                if cube.contains("red") {
+                    let amount = cube.split(' ').skip(1).next().unwrap();
+                    set.red(amount.parse().unwrap());
+                }
+                if cube.contains("green") {
+                    let amount = cube.split(' ').skip(1).next().unwrap();
+                    set.green(amount.parse().unwrap());
+                }
+                if cube.contains("blue") {
+                    let amount = cube.split(' ').skip(1).next().unwrap();
+                    set.blue(amount.parse().unwrap());
+                }
+            }
+
+            sets.push(set);
+        }
+
         Self {
             id,
-            sets: vec![],
+            sets,
         }
     }
 }
