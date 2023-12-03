081c28ea4032c76f9cfbec5aeabce3342a5eeca5
Eva Pace <eba.pachi@gmail.com>
2023-12-03 10:00:02 -0300

    3/rust: parse numbers

--- 3/main.rs
+++ 3/main.rs
@@ -1,22 +1,36 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::Path;
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+fn is_digit(ch: u8) -> bool {
+    ch >= 49 && ch <= 57
 }
 
 fn main() {
-    let lines = read_lines("./easy_input_part_one").unwrap(); // 4361
-    // let lines = read_lines("./input").unwrap();
+    let filename = "./easy_input_part_one"; // 4361
+    // let filename = "./input";
+    let file = File::open(filename).unwrap();
+    let curr = io::BufReader::new(&file).lines();
+    let mut prev = io::BufReader::new(&file).lines().into_iter();
+    let mut next = io::BufReader::new(&file).lines().into_iter().skip(1);
 
-    for line in lines {
+    let mut str_n = String::with_capacity(3);
+    // i, j (j == last digit of number)
+    let mut n_pos: (usize, usize) = (0, 0);
+    let mut n: u16;
+
+    for (i, line) in curr.enumerate() {
         let line = line.unwrap();
-        for cell in line.bytes() {
-            print!("{}", cell as char);
+        for (j, cell) in line.bytes().enumerate() {
+            if is_digit(cell) {
+                str_n.push(cell as char);
+                n_pos = (i, j);
+            } else if !str_n.is_empty() {
+                n = str_n.parse().unwrap();
+                str_n = "".to_owned();
+                println!("({},{}) {}", n_pos.0, n_pos.1, n);
+            }
         }
-        println!();
+        prev.next();
+        next.next();
     }
 }
