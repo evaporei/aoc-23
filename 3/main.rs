27922b4cc12bd476b9eb470287f593cd053d16a1
Eva Pace <eba.pachi@gmail.com>
2023-12-03 12:27:47 -0300

    3/rust: find * close to numbers

--- 3/main.rs
+++ 3/main.rs
@@ -9,6 +9,10 @@ fn is_symbol(ch: u8) -> bool {
     !is_digit(ch) && ch != b'.'
 }
 
+fn is_asterisk(ch: u8) -> bool {
+    ch == b'*'
+}
+
 type Pos = (usize, usize);
 
 fn has_symbol_same_line(curr_line: &str, pos: Pos, n_digits: usize) -> bool {
@@ -61,9 +65,59 @@ fn has_symbol_next_line(next: &Option<&String>, pos: Pos, n_digits: usize) -> bo
     false
 }
 
+fn has_digit_same_line(curr_line: &str, pos: Pos) -> bool {
+    // prev
+    if let Some(prev_pos) = pos.1.checked_sub(1) {
+        if let Some(ch) = curr_line.chars().nth(prev_pos) {
+            if is_digit(ch as u8) {
+                return true;
+            }
+        }
+    }
+    // next
+    if let Some(ch) = curr_line.chars().nth(pos.1 + 1) {
+        return is_digit(ch as u8);
+    }
+    false
+}
+
+fn has_digit_prev_line(prev: &Option<String>, pos: Pos) -> bool {
+    let prev = match prev {
+        Some(prev) => prev,
+        None => return false,
+    };
+    for p in 0..1 + 2 {
+        if let Some(prev_pos) = (pos.1 + p).checked_sub(1) {
+            if let Some(ch) = prev.chars().nth(prev_pos) {
+                if is_digit(ch as u8) {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
+fn has_digit_next_line(next: &Option<&String>, pos: Pos) -> bool {
+    let next = match next {
+        Some(next) => next,
+        None => return false,
+    };
+    for p in 0..1 + 2 {
+        if let Some(next_pos) = (pos.1 + p).checked_sub(1) {
+            if let Some(ch) = next.chars().nth(next_pos) {
+                if is_digit(ch as u8) {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
 fn main() {
-    // let filename = "./easy_input_part_one"; // 4361
-    let filename = "./input"; // 520019
+    let filename = "./easy_input_part_one"; // 4361, 467835
+    // let filename = "./input"; // 520019
     let file = File::open(filename).unwrap();
     let file2 = File::open(filename).unwrap();
     let curr = io::BufReader::new(&file).lines();
@@ -78,6 +132,7 @@ fn main() {
     let mut n_pos: Pos = (0, 0);
     let mut n: u16;
     let mut total: u32 = 0;
+    let mut total2: u32 = 0;
 
     // .........
     // ...456...
@@ -97,6 +152,7 @@ fn main() {
     for (i, line) in curr.enumerate() {
         let line = line.unwrap();
         for (j, cell) in line.bytes().enumerate() {
+            // part one
             if is_digit(cell) {
                 str_n.push(cell as char);
                 n_pos = (i, j);
@@ -112,6 +168,24 @@ fn main() {
                    has_symbol_prev_line(&prev, n_pos, n_digits) ||
                    has_symbol_next_line(&next.as_ref().map(|n| n.as_ref().unwrap()), n_pos, n_digits) {
                     total += n as u32;
+                }
+            }
+
+            // part two
+            if is_asterisk(cell) {
+                let ast_pos = (i, j);
+                // println!("({},{})", ast_pos.0, ast_pos.1);
+                let cur = has_digit_same_line(&line, ast_pos);
+                let pre = has_digit_prev_line(&prev, ast_pos);
+                let nxt = has_digit_next_line(&next.as_ref().map(|n| n.as_ref().unwrap()), ast_pos);
+                // println!("curr {}", cur);
+                // println!("prev {}", pre);
+                // println!("next {}", nxt);
+                // at least two
+                if (cur && pre) || (cur && nxt) || (pre && nxt) {
+                    // I'll have to parse the numbers ;-;
+                    // total2 *= n as u32;
+                    println!("BOOM");
                 }
             }
         }
