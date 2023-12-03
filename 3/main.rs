f8b97d7b3b13515ecebddc946601cbea1092c42f
Eva Pace <eba.pachi@gmail.com>
2023-12-03 19:00:51 -0300

    3/rust: use a vec (still same result) ;-;

--- 3/main.rs
+++ 3/main.rs
@@ -65,56 +65,71 @@ fn has_symbol_next_line(next: &Option<&String>, pos: Pos, n_digits: usize) -> bo
     false
 }
 
-fn has_digit_same_line(curr_line: &str, pos: Pos) -> Option<(Pos, char)> {
+// it should be a list instead of one Pos
+// prev:
+// if len(3):
+//   1 number, parse
+// if len(2):
+//   if together?
+//     1 number, search and get n
+//   else
+//     2 numbers, search each and get their n's
+// if len(1):
+//   1 number, search and get n
+
+fn has_digit_same_line(curr_line: &str, pos: Pos) -> Vec<(Pos, char)> {
+    let mut l = vec![];
     // prev
     if let Some(prev_pos) = pos.1.checked_sub(1) {
         if let Some(ch) = curr_line.chars().nth(prev_pos) {
             if is_digit(ch as u8) {
-                return Some(((pos.0, pos.1 - 1), ch));
+                l.push(((pos.0, pos.1 - 1), ch));
             }
         }
     }
     // next
     if let Some(ch) = curr_line.chars().nth(pos.1 + 1) {
         if is_digit(ch as u8) {
-            return Some(((pos.0, pos.1 + 1), ch));
+            l.push(((pos.0, pos.1 + 1), ch));
         }
     }
-    None
+    l
 }
 
-fn has_digit_prev_line(prev: &Option<String>, pos: Pos) -> Option<(Pos, char)> {
+fn has_digit_prev_line(prev: &Option<String>, pos: Pos) -> Vec<(Pos, char)> {
+    let mut l = vec![];
     let prev = match prev {
         Some(prev) => prev,
-        None => return None,
+        None => return l,
     };
     for p in 0..1 + 2 {
         if let Some(prev_pos) = (pos.1 + (p)).checked_sub(1) {
             if let Some(ch) = prev.chars().nth(prev_pos) {
                 if is_digit(ch as u8) {
-                    return Some(((pos.0 - 1, pos.1 + p - 1), ch));
+                    l.push(((pos.0 - 1, pos.1 + p - 1), ch));
                 }
             }
         }
     }
-    None
+    l
 }
 
-fn has_digit_next_line(next: &Option<&String>, pos: Pos) -> Option<(Pos, char)> {
+fn has_digit_next_line(next: &Option<&String>, pos: Pos) -> Vec<(Pos, char)> {
+    let mut l = vec![];
     let next = match next {
         Some(next) => next,
-        None => return None,
+        None => return l,
     };
     for p in 0..1 + 2 {
         if let Some(next_pos) = (pos.1 + p).checked_sub(1) {
             if let Some(ch) = next.chars().nth(next_pos) {
                 if is_digit(ch as u8) {
-                    return Some(((pos.0 + 1, pos.1 + p - 1), ch));
+                    l.push(((pos.0 + 1, pos.1 + p - 1), ch));
                 }
             }
         }
     }
-    None
+    l
 }
 
 type Data = (Pos, u16, String);
@@ -237,33 +252,90 @@ fn main() {
         for (j, cell) in line.bytes().enumerate() {
             if is_asterisk(cell) {
                 let mut ratio: u32 = 1;
-                let ast_pos = (i, j);
+                let ast_pos = dbg!((i, j));
                 // println!("({},{})", ast_pos.0, ast_pos.1);
                 let cur = has_digit_same_line(&line, ast_pos);
                 let pre = has_digit_prev_line(&prev, ast_pos);
                 let nxt = has_digit_next_line(&next.as_ref().map(|n| n.as_ref().unwrap()), ast_pos);
-                // println!("curr {:?}", cur);
-                // println!("prev {:?}", pre);
-                // println!("next {:?}", nxt);
-                if let Some((pos, ch)) = cur {
-                    if let Some(found) = numbers.get(pos, ch) {
-                        ratio *= found as u32;
+                println!("curr {:?}", cur);
+                println!("prev {:?}", pre);
+                println!("next {:?}", nxt);
+                if cur.len() == 2 {
+                    println!("cur = 2");
+                    // find
+                    if let Some(found) = numbers.get(cur[0].0, cur[0].1) {
+                        ratio *= dbg!(found as u32);
+                    }
+                    if let Some(found) = numbers.get(cur[1].0, cur[1].1) {
+                        ratio *= dbg!(found as u32);
+                    }
+                } else if cur.len() == 1 {
+                    println!("cur = 1");
+                    if let Some(found) = numbers.get(cur[0].0, cur[0].1) {
+                        ratio *= dbg!(found as u32);
                     }
                 }
-                if let Some((pos, ch)) = pre {
-                    if let Some(found) = numbers.get(pos, ch) {
-                        ratio *= found as u32;
+
+                // it's one number w/ three digits
+                if pre.len() == 3 {
+                    println!("pre = 3");
+                    // we could directly parse here
+                    if let Some(found) = numbers.get(pre[2].0, pre[2].1) {
+                        ratio *= dbg!(found as u32);
+                    }
+                } else if pre.len() == 2 {
+                    println!("pre = 2");
+                    // together?
+                    if pre[1].0.1 - pre[0].0.1 == 1 {
+                        if let Some(found) = numbers.get(pre[1].0, pre[1].1) {
+                            ratio *= dbg!(found as u32);
+                        }
+                    } else {
+                        if let Some(found) = numbers.get(pre[0].0, pre[0].1) {
+                            ratio *= dbg!(found as u32);
+                        }
+                        if let Some(found) = numbers.get(pre[1].0, pre[1].1) {
+                            ratio *= dbg!(found as u32);
+                        }
+                    }
+                } else if pre.len() == 1 {
+                    if let Some(found) = numbers.get(pre[0].0, pre[0].1) {
+                        ratio *= dbg!(found as u32);
                     }
                 }
-                if let Some((pos, ch)) = nxt {
-                    if let Some(found) = numbers.get(pos, ch) {
-                        ratio *= found as u32;
+
+                // it's one number w/ three digits
+                if nxt.len() == 3 {
+                    println!("nxt = 3");
+                    // we could directly parse here
+                    if let Some(found) = numbers.get(nxt[2].0, nxt[2].1) {
+                        ratio *= dbg!(found as u32);
+                    }
+                } else if nxt.len() == 2 {
+                    println!("nxt = 2");
+                    // together?
+                    if nxt[1].0.1 - nxt[0].0.1 == 1 {
+                        if let Some(found) = numbers.get(nxt[1].0, nxt[1].1) {
+                            ratio *= dbg!(found as u32);
+                        }
+                    } else {
+                        if let Some(found) = numbers.get(nxt[0].0, nxt[0].1) {
+                            ratio *= dbg!(found as u32);
+                        }
+                        if let Some(found) = numbers.get(nxt[1].0, nxt[1].1) {
+                            ratio *= dbg!(found as u32);
+                        }
+                    }
+                } else if nxt.len() == 1 {
+                    println!("nxt = 1");
+                    if let Some(found) = numbers.get(nxt[0].0, nxt[0].1) {
+                        ratio *= dbg!(found as u32);
                     }
                 }
                 // at least two
-                if (cur.is_some() && pre.is_some()) || (cur.is_some() && nxt.is_some()) || (pre.is_some() && nxt.is_some()) {
-
+                if (!cur.is_empty() && !pre.is_empty()) || (!cur.is_empty() && !nxt.is_empty()) || (!pre.is_empty() && !nxt.is_empty()) {
                     if ratio != 1 {
+                        println!("f o i");
                         total2 += ratio as u32;
                     }
                 }
