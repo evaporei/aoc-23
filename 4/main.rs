14bf51133b2de7045297b68077b4db8159bbf479
Eva Pace <eba.pachi@gmail.com>
2023-12-04 05:19:51 -0300

    4/rust: finish first part

--- 4/main.rs
+++ 4/main.rs
@@ -1,3 +1,4 @@
+use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -8,19 +9,16 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn parse_number_list(list: &str) -> Vec<u8> {
-    let mut l = vec![];
+// they're actually sets instead of lists
+fn parse_number_list(list: &str) -> BTreeSet<u8> {
+    let mut l = BTreeSet::new();
     // example input: " 41 48 83 86 17 "
     // ["41", "48", "83", "86", "17"]
-    let numbers = list.trim().split(' ');
+    let numbers = list.trim().split_whitespace();
 
     // "41" | "48" | "83" | ...
     for n in numbers {
-        // there could be an empty space because of a
-        // single digit number
-        if let Ok(n) = n.parse() {
-            l.push(n);
-        }
+        l.insert(n.parse().unwrap());
     }
 
     l
@@ -29,14 +27,14 @@ fn parse_number_list(list: &str) -> Vec<u8> {
 #[derive(Debug)]
 struct Card {
     id: u16,
-    winning: Vec<u8>, // [10; u8] (5 in example input)
-    have: Vec<u8>, // [25; u8] (8 in example input)
+    winning: BTreeSet<u8>, // [10; u8] (5 in example input)
+    have: BTreeSet<u8>, // [25; u8] (8 in example input)
 }
 
 impl Card {
     fn parse(line: &str) -> Self {
-        // example input: "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-        let id = line.split(' ') // ["Card", "1:", ...]
+        // example input: "Card   1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+        let id = line.split_whitespace() // ["Card", "1:", ...]
             .skip(1) // ["1:", ...]
             .next() // "1:"
             .unwrap();
@@ -65,11 +63,29 @@ impl Card {
 }
 
 fn main() {
-    let lines = read_lines("./easy_input_part_one").unwrap(); // 13
-    // let lines = read_lines("./input").unwrap();
+    // let lines = read_lines("./easy_input_part_one").unwrap(); // 13
+    let lines = read_lines("./input").unwrap(); // 32609
+    let mut points = 0;
+    let mut lens = vec![];
+
     for line in lines {
         let line = line.unwrap();
         let card = Card::parse(&line);
-        dbg!(card);
+
+        let intersection: Vec<_> = card
+            .winning
+            .intersection(&card.have)
+            .cloned()
+            .collect();
+
+        lens.push(intersection.len());
     }
+
+    for len in lens {
+        if let Some(l) = len.checked_sub(1) {
+            points += 1 << l;
+        }
+    }
+
+    println!("{points}");
 }
