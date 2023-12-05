68c2cfbf6f62dfc17348945e96eb136314574e1d
Eva Pace <eba.pachi@gmail.com>
2023-12-05 06:24:52 -0300

    5/rust: uhh tests first cause I'm gay

--- 5/main.rs
+++ 5/main.rs
@@ -8,6 +8,40 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+// input could be seed, soil, etc
+fn dst_from_src(input: u32, dst: u32, src: u32, range: u32) -> u32 {
+    if src > input || src + range < input {
+        return input;
+    }
+
+    // it could be (input - src) == 0
+    if src == input {
+        return dst;
+    }
+    // for (dst_n, src_n) in (dst..dst+range).iter().zip((src..src+range).iter()) {
+    // for dst_n in dst..dst+range {
+    // }
+    let diff = input - src;
+    dst + diff
+}
+
+#[test]
+fn test_src_to_dst() {
+    let seed = 100;
+    // 120 > 100, there's nothing to range, input returns
+    assert_eq!(dst_from_src(seed, 13, 120, 50), 100);
+    // 100 == 100, return first dst
+    assert_eq!(dst_from_src(seed, 50, 100, 2), 50);
+    // 97 + 2 = 99, still lower than 100, return input
+    assert_eq!(dst_from_src(seed, 50, 97, 2), 100);
+
+    // succeeds
+    assert_eq!(dst_from_src(seed, 13, 55, 50), 58);
+
+    // boundary checks
+    assert_eq!(dst_from_src(seed, 13, 95, 5), 18);
+}
+
 fn parse_seeds(line: &str) -> Vec<u32> {
     let numbers = line // "seeds: 79 14 55 13"
         .split(':') // ["seeds", " 79 14 55 13"]
