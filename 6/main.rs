9f3dfa2be742e335e31e0cb817136a3c02cf1935
Eva Pace <eba.pachi@gmail.com>
2023-12-06 07:21:04 -0300

    6/rust: finish part one

--- 6/main.rs
+++ 6/main.rs
@@ -8,9 +8,30 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+fn n_ways_to_beat(time: u16, record: u16) -> u16 {
+    let mut n = 0;
+
+    for hold in 1..time {
+        let time_left = time - hold;
+        let distance_travelled = hold * time_left;
+        if distance_travelled > record {
+            n += 1;
+        }
+    }
+
+    n
+}
+
+#[test]
+fn test_n_ways_to_beat() {
+    assert_eq!(n_ways_to_beat(7, 9), 4);
+    assert_eq!(n_ways_to_beat(15, 40), 8);
+    assert_eq!(n_ways_to_beat(30, 200), 9);
+}
+
 fn main() {
-    let mut lines = read_lines("./easy_input_part_one").unwrap(); // 35
-    // let mut lines = read_lines("./input").unwrap();
+    // let mut lines = read_lines("./easy_input_part_one").unwrap(); // 288
+    let mut lines = read_lines("./input").unwrap(); // 840336
 
     // parsing
     let time_str = lines.next().unwrap().unwrap();
@@ -27,7 +48,11 @@ fn main() {
         .map(str::parse)
         .map(Result::unwrap)
         .collect();
+    let mut total: u32 = 1;
 
-    dbg!(times);
-    dbg!(distances);
+    for (time, distance) in times.iter().zip(distances.iter()) {
+        total *= n_ways_to_beat(*time, *distance) as u32;
+    }
+
+    println!("part one {total}");
 }
