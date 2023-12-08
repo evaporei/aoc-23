7bffb35b90a73f9c63c0cc2e0d4e4319ab6fa693
Eva Pace <eba.pachi@gmail.com>
2023-12-08 16:57:31 -0300

    8: it does work w/ the example, (why lcm doesn't work?) 🤔

---
+++ 8/lcm.py
@@ -0,0 +1,9 @@
+from math import lcm
+
+# 201405124411
+print(lcm(18157, 14363, 16531, 12737, 19783, 18157))
+# w/out the first one? -> same.
+print(lcm(14363, 16531, 12737, 19783, 18157))
+
+# obviously, 6
+print(lcm(2, 3))
--- 8/main.rs
+++ 8/main.rs
@@ -12,7 +12,9 @@ where P: AsRef<Path>, {
 fn main() {
     // let mut lines = read_lines("./example1").unwrap(); // 2
     // let mut lines = read_lines("./example2").unwrap(); // 6
-    let mut lines = read_lines("./input").unwrap(); // 18157, 19783 (too low)
+    let mut lines = read_lines("./example_part_two").unwrap(); // 6
+    // let mut lines = read_lines("./input").unwrap(); // 18157, 19783 (too low),
+    // 201405124411 (lcm, wrong ;-;)
 
     // LLR
     let steps = lines.next().unwrap().unwrap();
@@ -21,7 +23,8 @@ fn main() {
     let _ = lines.next();
 
     // let a_steps = ["AAA", "RLA", "QLA", "QFA", "RXA", "JSA"];
-    let z_steps = ["QCZ", "LRZ", "ZZZ", "PQZ", "VHZ", "JJZ"];
+    // let z_steps = ["QCZ", "LRZ", "ZZZ", "PQZ", "VHZ", "JJZ"];
+    let z_steps = ["11Z", "22Z"];
 
     let mut map = BTreeMap::new();
 
@@ -51,36 +54,43 @@ fn main() {
     }
 
     // let n_steps = find_steps("AAA", "ZZZ", &map, &steps);
-    let n_steps = find_steps("AAA", &["ZZZ"], &map, &steps);
-    println!("part one {n_steps}");
+    // let n_steps = find_steps("AAA", &["ZZZ"], &map, &steps);
+    // println!("part one {n_steps}");
 
     // for a_step in a_steps {
     //     let n_steps = find_steps(a_step, &z_steps, &map, &steps);
     //     println!("part two {a_step}: {n_steps}");
     // }
     println!("part two:");
-    let n_steps = find_steps("AAA", &["ZZZ"], &map, &steps);
-    println!("{} - {}: {n_steps}", "AAA", "ZZZ"); // 18157
-    let n_steps = find_steps("RLA", &["JJZ"], &map, &steps);
-    println!("{} - {}: {n_steps}", "RLA", "JJZ"); // 14363
-    let n_steps = find_steps("QLA", &["VHZ"], &map, &steps);
-    println!("{} - {}: {n_steps}", "QLA", "VHZ"); // 16531
-    let n_steps = find_steps("QFA", &["PQZ"], &map, &steps);
-    println!("{} - {}: {n_steps}", "QFA", "PQZ"); // 12737
-    let n_steps = find_steps("RXA", &["QCZ"], &map, &steps);
-    println!("{} - {}: {n_steps}", "RXA", "QCZ"); // 19783 -> shouldn't be the answer? (max)
-    let n_steps = find_steps("AAA", &["LRZ"], &map, &steps);
-    println!("{} - {}: {n_steps}", "JSA", "LRZ"); // 18157
+    // let n_steps = find_steps("AAA", &["ZZZ"], &map, &steps);
+    // println!("{} - {}: {n_steps}", "AAA", "ZZZ"); // 18157
+    // let n_steps = find_steps("RLA", &["JJZ"], &map, &steps);
+    // println!("{} - {}: {n_steps}", "RLA", "JJZ"); // 14363
+    // let n_steps = find_steps("QLA", &["VHZ"], &map, &steps);
+    // println!("{} - {}: {n_steps}", "QLA", "VHZ"); // 16531
+    // let n_steps = find_steps("QFA", &["PQZ"], &map, &steps);
+    // println!("{} - {}: {n_steps}", "QFA", "PQZ"); // 12737
+    // let n_steps = find_steps("RXA", &["QCZ"], &map, &steps);
+    // println!("{} - {}: {n_steps}", "RXA", "QCZ"); // 19783 (max)
+    // let n_steps = find_steps("AAA", &["LRZ"], &map, &steps);
+    // println!("{} - {}: {n_steps}", "JSA", "LRZ"); // 18157
+
+    // example part two
+    let n_steps = find_steps("11A", &["11Z"], &map, &steps);
+    println!("{} - {}: {n_steps}", "11A", "11Z"); // 18157
+    let n_steps = find_steps("22A", &["22Z"], &map, &steps);
+    println!("{} - {}: {n_steps}", "22A", "22Z"); // 18157
 }
 
 // fn find_steps(start: &str, end: &str, map: &BTreeMap<String, (String, String)>, steps: &str) -> u32 {
-fn find_steps(start: &str, ends: &[&str], map: &BTreeMap<String, (String, String)>, steps: &str) -> u32 {
+fn find_steps(start: &str, ends: &[&str], map: &BTreeMap<String, (String, String)>, steps: &str) -> u64 {
     let (mut l, mut r) = map.get(start).unwrap().clone();
     let mut n_steps = 1;
     let mut i = 0;
 
     // while l != end || r != end {
     while !ends.contains(&l.as_str()) || !ends.contains(&r.as_str()) {
+    // while n_steps <= 201_405_124_411 {
         if i == steps.len() {
             i = 0;
         }
