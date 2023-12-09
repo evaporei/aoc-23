239ed678122532010b438aa7fe8e89c5f847d3f6
Eva Pace <eba.pachi@gmail.com>
2023-12-09 15:46:44 -0300

    9: finish part 2

--- 9/main.rs
+++ 9/main.rs
@@ -9,9 +9,10 @@ where P: AsRef<Path>, {
 }
 
 fn main() {
-    // let lines = read_lines("./example_part_one").unwrap(); // 114
-    let lines = read_lines("./input").unwrap(); // 1930746032
+    // let lines = read_lines("./example_part_one").unwrap(); // 114, 2
+    let lines = read_lines("./input").unwrap(); // 1930746032, 1154
     let mut extrapolated_sum = 0;
+    let mut extrapolated_backwards = 0;
 
     for line in lines {
         let line = line.unwrap();
@@ -27,9 +28,15 @@ fn main() {
 
         let new_history = lists[0][lists[0].len() - 1];
         extrapolated_sum += new_history;
+
+        add_history_backwards(&mut lists);
+
+        let backwards_history = lists[0][0];
+        extrapolated_backwards += backwards_history;
     }
 
     println!("part one {extrapolated_sum}");
+    println!("part two {extrapolated_backwards}");
 }
 
 fn diff(mut start: Vec<Vec<i32>>, numbers: Vec<i32>) -> Vec<Vec<i32>> {
@@ -70,6 +77,32 @@ fn add_new_history(lists: &mut Vec<Vec<i32>>) {
         let sum = curr_last_elem + curr_to_add;
         curr_to_add = sum;
         lists[rev_i].push(sum);
+        if rev_i == 0 {
+            break;
+        }
+        rev_i -= 1;
+    }
+}
+
+// adds a history backwards to the first list (input)
+fn add_history_backwards(lists: &mut Vec<Vec<i32>>) {
+    // insert a repeating 3 or 2 or 1
+    // they're all the same in the last list
+    let last_elem_idx = lists.len() - 1;
+    let last_list = &mut lists[last_elem_idx];
+    let mut curr_to_sub = last_list[0];
+    last_list.insert(0, curr_to_sub);
+
+    let old_len = lists[0].len();
+
+    let mut rev_i = lists.len() - 2;
+
+    // we push to the first list, this loop ends
+    while old_len == lists[0].len() {
+        let curr_first_elem = lists[rev_i][0];
+        let sub = curr_first_elem - curr_to_sub;
+        curr_to_sub = sub;
+        lists[rev_i].insert(0, sub);
         if rev_i == 0 {
             break;
         }
