9c23af546a73f20bfbfcd4fd490a027af2c1e657
Eva Pace <eba.pachi@gmail.com>
2023-12-11 21:32:44 -0300

    11: collect space map (matrix)

--- 11/main.rs
+++ 11/main.rs
@@ -8,15 +8,24 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+// I've always wanted to have a type w/ Space in it's name lol
+type SpaceMap = Vec<Vec<u8>>;
+
 // const FILENAME: &str = "./example_input";
 const FILENAME: &str = "./input";
 
 fn main() {
     let lines = read_lines(FILENAME).unwrap();
+    let map = collect_map(lines);
+}
+
+fn collect_map(lines: io::Lines<io::BufReader<File>>) -> SpaceMap {
+    let mut map = Vec::with_capacity(151);
 
     for line in lines {
         let line = line.unwrap();
-
-        println!("{line}");
+        map.push(line.bytes().collect());
     }
+
+    map
 }
