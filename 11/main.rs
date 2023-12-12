44d68708309647970d3dd1985fa93a192bd30760
Eva Pace <eba.pachi@gmail.com>
2023-12-12 06:07:18 -0300

    11: expand empty lines/columns in map

--- 11/main.rs
+++ 11/main.rs
@@ -11,12 +11,23 @@ where P: AsRef<Path>, {
 // I've always wanted to have a type w/ Space in it's name lol
 type SpaceMap = Vec<Vec<u8>>;
 
+#[allow(unused)]
+fn print_map(map: &SpaceMap) {
+    for line in map {
+        for ch in line {
+            print!("{}", *ch as char);
+        }
+        println!();
+    }
+}
+
 // const FILENAME: &str = "./example_input";
 const FILENAME: &str = "./input";
 
 fn main() {
     let lines = read_lines(FILENAME).unwrap();
-    let map = collect_map(lines);
+    let mut map = collect_map(lines);
+    expand(&mut map);
 }
 
 fn collect_map(lines: io::Lines<io::BufReader<File>>) -> SpaceMap {
@@ -28,4 +39,48 @@ fn collect_map(lines: io::Lines<io::BufReader<File>>) -> SpaceMap {
     }
 
     map
+}
+
+fn is_empty(slice: &[u8]) -> bool {
+    slice.iter().all(|ch| *ch == b'.')
+}
+
+fn expand(map: &mut SpaceMap) {
+    let mut horizontal_idxs = vec![];
+    for i in 0..map.len() {
+        let line = &map[i];
+        if is_empty(line) {
+            horizontal_idxs.push(i);
+        }
+    }
+
+    while !horizontal_idxs.is_empty() {
+        map.insert(
+            *horizontal_idxs.last().unwrap(),
+            std::iter::repeat(b'.').take(map[0].len()).collect::<Vec<u8>>()
+        );
+        horizontal_idxs.pop();
+    }
+
+    let mut vertical_idxs = vec![];
+    for j in 0..map[0].len() {
+        let mut all_dot = true;
+        for i in 0..map.len() {
+            if map[i][j] != b'.' {
+                all_dot = false;
+                break;
+            }
+        }
+        if all_dot {
+            vertical_idxs.push(j);
+        }
+    }
+
+    while !vertical_idxs.is_empty() {
+        let last = vertical_idxs.last().unwrap();
+        for i in 0..map.len() {
+            map[i].insert(*last, b'.');
+        }
+        vertical_idxs.pop();
+    }
 }
