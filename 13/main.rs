d4fa59090506897f89d0930a416364834f5420d9
Eva Pace <eba.pachi@gmail.com>
2023-12-13 08:05:01 -0300

    13: encode horizontal lines

--- 13/main.rs
+++ 13/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 // . -> 0
 // # -> 1
 fn encode(s: &str) -> u32 {
@@ -11,5 +21,25 @@ fn test_encode() {
     assert_eq!(encode(".###.#."), 0b0111010);
 }
 
+const FILENAME: &str = "./example_input";
+// const FILENAME: &str = "./input";
+
 fn main() {
+    let lines = read_lines(FILENAME).unwrap();
+    let mut h_patterns = vec![vec![]];
+
+    for line in lines {
+        let line = line.unwrap();
+
+        if !line.is_empty() {
+            if let Some(p) = h_patterns.last_mut() {
+                p.push(encode(&line));
+            }
+        } else {
+            h_patterns.push(vec![]);
+        }
+    }
+
+    dbg!(&h_patterns);
+    dbg!(&h_patterns.len());
 }
