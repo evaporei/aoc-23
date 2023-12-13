e67da1555520c7c6d8a29693156736fc784367a1
Eva Pace <eba.pachi@gmail.com>
2023-12-13 08:21:16 -0300

    13: vertical columns seem to be encoded too
    matrices scare me 😱

--- 13/main.rs
+++ 13/main.rs
@@ -21,12 +21,16 @@ fn test_encode() {
     assert_eq!(encode(".###.#."), 0b0111010);
 }
 
-const FILENAME: &str = "./example_input";
-// const FILENAME: &str = "./input";
+// const FILENAME: &str = "./example_input";
+// const MAX_LINE_LEN: usize = 9;
+
+const FILENAME: &str = "./input";
+const MAX_LINE_LEN: usize = 17;
 
 fn main() {
     let lines = read_lines(FILENAME).unwrap();
     let mut h_patterns = vec![vec![]];
+    let mut v_patterns = vec![vec![String::with_capacity(20); MAX_LINE_LEN]];
 
     for line in lines {
         let line = line.unwrap();
@@ -35,11 +39,29 @@ fn main() {
             if let Some(p) = h_patterns.last_mut() {
                 p.push(encode(&line));
             }
+            if let Some(p) = v_patterns.last_mut() {
+                for (j, ch) in line.chars().enumerate() {
+                    p[j].push(ch);
+                }
+            }
         } else {
             h_patterns.push(vec![]);
+            v_patterns.push(vec![String::with_capacity(20); MAX_LINE_LEN]);
         }
     }
 
-    dbg!(&h_patterns);
-    dbg!(&h_patterns.len());
+    // remove empty strings because we made a square
+    // matrix based of the maximum line length
+    let v_patterns: Vec<Vec<u32>> = v_patterns.into_iter()
+        .map(|l| l.into_iter().filter_map(|s| {
+            if s.is_empty() {
+                None
+            } else {
+                Some(encode(&s))
+            }
+        }).collect())
+        .collect();
+
+    dbg!(&v_patterns);
+    dbg!(&v_patterns.len());
 }
